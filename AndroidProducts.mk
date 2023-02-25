15e02f665b6f2acca1379406cd68e1b6c94ca8ac
hardy <hardy@MacBook-Pro.local>
2023-02-25 19:26:31 +0800

    add B2G

---
+++ AndroidProducts.mk
@@ -0,0 +1,5 @@
+PRODUCT_MAKEFILES := \
+	$(LOCAL_DIR)/treble_arm64_avN.mk \
+	$(LOCAL_DIR)/treble_arm64_bvN.mk \
+
+
--- base-sas.mk
+++ base-sas.mk
@@ -1,11 +1,6 @@
 PRODUCT_COPY_FILES += \
 	device/phh/treble/apex-setup.rc:system/etc/init/apex-setup.rc \
 
-PRODUCT_COPY_FILES += \
-	system/core/rootdir/init.zygote32.rc:system/etc/init/zygote/init.zygote32.rc \
-	system/core/rootdir/init.zygote32_64.rc:system/etc/init/zygote/init.zygote32_64.rc \
-	system/core/rootdir/init.zygote64_32.rc:system/etc/init/zygote/init.zygote64_32.rc \
-	system/core/rootdir/init.zygote64.rc:system/etc/init/zygote/init.zygote64.rc \
 
 PRODUCT_COPY_FILES += \
 	frameworks/av/apex/mediaswcodec.rc:system/etc/init/mediaswcodec-treble.rc \
