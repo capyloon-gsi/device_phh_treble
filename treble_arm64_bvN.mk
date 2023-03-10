932a9e3b3cfe746661aa405b56b006ee3bef89fa
ittat <ittat@live.com>
2023-03-10 11:03:58 +0800

    change 'td' back 'phh'

--- treble_arm64_bvN.mk
+++ treble_arm64_bvN.mk
@@ -4,10 +4,10 @@ include build/make/target/product/aosp_arm64.mk
 $(call inherit-product, device/phh/treble/base.mk)
 
 PRODUCT_NAME := treble_arm64_bvN
-PRODUCT_DEVICE := tdgsi_arm64_ab
-PRODUCT_BRAND := google
-PRODUCT_SYSTEM_BRAND := google
-PRODUCT_MODEL := Capyloon GSI
+PRODUCT_DEVICE := phhgsi_arm64_ab
+PRODUCT_BRAND := Capyloon
+PRODUCT_SYSTEM_BRAND := Capyloon
+PRODUCT_MODEL := GSI
 
 # Overwrite the inherited "emulator" characteristics
 PRODUCT_CHARACTERISTICS := device
